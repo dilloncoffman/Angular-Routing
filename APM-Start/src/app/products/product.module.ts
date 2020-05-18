48877e0e82afe8aafdf64555902998dae2cdbe03
Dillon Coffman <tug40394@temple.edu>
2020-05-18 17:53:16 -0400

    Configured ProductModule routes for using Child Routes

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -7,6 +7,8 @@ import { ProductEditComponent } from "./product-edit/product-edit.component";
 import { SharedModule } from "../shared/shared.module";
 import { RouterModule } from "@angular/router";
 import { ProductResolverService } from "./product-resolver.service";
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
@@ -22,6 +24,11 @@ import { ProductResolverService } from "./product-resolver.service";
         path: "products/:id/edit",
         component: ProductEditComponent,
         resolve: { resolvedProductData: ProductResolverService },
+        children: [
+          { path: '', redirectTo: 'info', pathMatch: 'full' },
+          { path: 'info', component: ProductEditInfoComponent },
+          { path: 'tags', component: ProductEditTagsComponent }
+        ]
       },
     ]),
   ],
@@ -29,6 +36,8 @@ import { ProductResolverService } from "./product-resolver.service";
     ProductListComponent,
     ProductDetailComponent,
     ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ],
 })
 export class ProductModule {}
